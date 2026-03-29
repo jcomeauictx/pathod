38e201fec0787fd9d6e001455ceb780eae12691a
John Comeau <jc@unternet.net>
2026-03-29 03:38:28 +0000

    these should probably be bytes too

--- libpathod/pathod.py
+++ libpathod/pathod.py
@@ -65,9 +65,9 @@ class PathodHandler(tcp.BaseHandler):
             log: A dictionary, or None
         """
         line = self.rfile.readline()
-        if line == "\r\n" or line == "\n": # Possible leftover from previous message
+        if line in (b'\r\n', b'\n'): # Possible leftover from previous message
             line = self.rfile.readline()
-        if line == "":
+        if line == b'':
             # Normal termination
             return False, None
 
@@ -75,9 +75,9 @@ class PathodHandler(tcp.BaseHandler):
         if m(http.parse_init_connect(line)):
             headers = http.read_headers(self.rfile)
             self.wfile.write(
-                        'HTTP/1.1 200 Connection established\r\n' +
-                        ('Proxy-agent: %s\r\n'%version.NAMEVERSION) +
-                        '\r\n'
+                        b'HTTP/1.1 200 Connection established\r\n' +
+                        (b'Proxy-agent: %s\r\n'%version.NAMEVERSION) +
+                        b'\r\n'
                         )
             self.wfile.flush()
             if not self.server.ssloptions.not_after_connect:
